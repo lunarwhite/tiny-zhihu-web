9830622a8f43189ef4f41e3f39b7b6bba28c9747
lunarwhite <dwcn22@outlook.com>
2021-05-12 09:00:45 +0800

    update db config file

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import  os
 
-HOSTNAME = '127.0.0.1'
-PORT     = '1433'
-DATABASE = 'izhihu'
-USERNAME = 'sa'
-PASSWORD = '*tx720618'
+HOSTNAME = '127.0.0.1' # 主机内网ip
+PORT     = '1433' # 端口
+DATABASE = 'izhihu' # 数据库名称
+USERNAME = 'sa' # 用户名
+PASSWORD = '*tx720618' # 密码
 
-DB_URI = 'mssql+pymssql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
+DB_URI = 'mssql+pymssql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
+
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 DEBUG = True
 
-SECRET_KEY = os.urandom(24)
+SECRET_KEY = os.urandom(24) # 生成24位随机字符
